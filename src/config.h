3003405f29dea7e41ec9b25c20c9aed5e4622f0a
Victoria Wu <vicky.ptarmigan@gmail.com>
2013-02-02 15:26:37 -0600

    config.h is now for global stuff. it's included in more localized config

--- src/config.h
+++ src/config.h
@@ -1,8 +1,7 @@
-#ifndef SPATIAL_CONFIG_H
-#define SPATIAL_CONFIG_H
+#ifndef CONFIG_H
+#define CONFIG_H
 
-const double GYRO_OFFSET[3] = {-0.241803, 0.02563817, -0.3029};
-const int DATA_RATE = 16;	//phidget pushes packets every 16 millisec
+#define DATA_RATE = 16;	//phidget pushes packets every 16 millisec
 
 /*SPATIAL_CONFIG_H*/
 #endif

e254bec383beefc3208c9b76ad12550906e75d69
Robocode2 <umkc@robocode2.(none)>
2013-02-08 23:53:03 -0600

    populated service message request w/fake data

--- src/example_publishOrientation.cpp
+++ src/example_publishOrientation.cpp
@@ -10,6 +10,14 @@ int main(int argc, char **argv)	{
 	ros::ServiceClient client = n.serviceClient<IMU::imu_filter>("Calculate_Orientation");
 	
 	IMU::imu_filter srv;
+	srv.request.rawIMU.a_x = -0.01244;
+	srv.request.rawIMU.a_y = -0.00801;
+	srv.request.rawIMU.a_z = .98175;
+	srv.request.rawIMU.w_x = -1.88324;
+	srv.request.rawIMU.w_x = -.59418;
+	srv.request.rawIMU.w_x = .10736;
+
+	srv.response.roll = 42.5;
 	if(client.call(srv))	{
 		ROS_INFO("Roll: %f", srv.response.roll);
 	}	

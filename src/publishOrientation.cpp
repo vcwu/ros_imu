6a47ba7122013943bc384e7c464488f8845f9ece
Robocode2 <umkc@robocode2.(none)>
2013-02-08 23:52:31 -0600

    Added berk imufilter code. Working.

--- src/publishOrientation.cpp
+++ src/publishOrientation.cpp
@@ -26,26 +26,10 @@ bool callback(std_servs::Empty::Request&, std_srvs::Empty::Response& response)	{
 //BERKTESTER0 //includes needed for other BERKTESTSER code
 #endif
 
-bool calculate(IMU::imu_filter::Request &request, IMU::imu_filter::Response &response)
-{
-  //this is where the berk code goes
-  //Raw IMU data is request.rawIMU.*
-  // output is response.* <--these may stil need to be set in the srv file
 
-/*
-  double orientation[3], rotMatrix[3][3]; 
-  response.roll = deg_from_rad(imuFilter.getRoll());
-  response.pitch  = deg_from_rad(imuFilter.getPitch());
-  response.yaw = deg_from_rad(imuFilter.getYaw());
-*/
-//  imufilter.getRotationMatrix(response.rotMatrix);
+IMUfilter imuFilter(seconds_from_ms(DATA_RATE), gyroscopeErrorRate);
 
-#ifdef BERKTESTER
-//BERKTESTER2//print out any statements
-#endif
-
-  return true;
-}
+bool calculate(IMU::imu_filter::Request &request, IMU::imu_filter::Response &response);
 
 int main(int argc, char* argv[])
 {
@@ -54,13 +38,42 @@ int main(int argc, char* argv[])
 //BERKTESTER1 //define filestreams
 #endif //ifdef BERKTESTER
 
-	IMUfilter imuFilter(seconds_from_ms(DATA_RATE), gyroscopeErrorRate);
 	
 	ros::init(argc, argv, "Calculate_Orientation_server");
 	ros::NodeHandle berk;
-
-	ros::ServiceServer service = berk.advertiseService("Calculate_Orientation", calculate);
+	
+	imuFilter.reset();
+	ros::ServiceServer service = berk.advertiseService("Calculate_Orientation", calculate );
 	ROS_INFO("Ready to calculate orientation.");
+	ROS_INFO("Roll: %f, Pitch: %f, Yaw: %f", imuFilter.getRoll(),
+		imuFilter.getPitch(), imuFilter.getYaw());
 	ros::spin();
 	return 0;
 }
+
+bool calculate(IMU::imu_filter::Request &request, IMU::imu_filter::Response &response)
+{
+	//Update filter with IMU data
+	IMU::spatialRaw raw = request.rawIMU;
+	ROS_INFO("a_x: %f, a_y: %f, a_z:%f", raw.a_x, raw.a_y, raw.a_z);
+	ROS_INFO("w_x: %f, w_y: %f, w_z:%f", raw.w_x, raw.w_y, raw.w_z);
+	
+	imuFilter.updateFilter(raw.w_x, raw.w_y, raw.w_z,
+		raw.a_x, raw.a_y, raw.a_z);
+	imuFilter.computeEuler();
+  	double rotMatrix[3][3];
+
+	ROS_INFO("Roll: %f, Pitch: %f, Yaw: %f", imuFilter.getRoll(),
+		imuFilter.getPitch(), imuFilter.getYaw());
+  	response.roll = deg_from_rad(imuFilter.getRoll());
+  	response.pitch  = deg_from_rad(imuFilter.getPitch());
+  	response.yaw = deg_from_rad(imuFilter.getYaw());
+//  imufilter.getRotationMatrix(response.rotMatrix);
+
+#ifdef BERKTESTER
+//BERKTESTER2//print out any statements
+#endif
+	  return true;
+}
+
+

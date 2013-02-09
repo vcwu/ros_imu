884224f15e656bc726dd48da7d288d7b5345bb90
Robocode2 <umkc@robocode2.(none)>
2013-02-08 21:29:54 -0600

    fixed data_rate compiler error

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define DATA_RATE = 16;	//phidget pushes packets every 16 millisec
+#define DATA_RATE 16	//phidget pushes packets every 16 millisec
 
 /*SPATIAL_CONFIG_H*/
 #endif

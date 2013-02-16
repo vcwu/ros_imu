aaae9dd25ecfdf554213184b6b2e064bd0807036
Victoria Wu <vicky.ptarmigan@gmail.com>
2013-02-16 14:36:26 -0600

    Added more conversion methods (mss from gs

--- src/config.h
+++ src/config.h
@@ -3,6 +3,9 @@
 
 
 #define gyroscopeErrorRate  0.01
+
+#define mss_from_gs(X) ((X)*9.8)
+#define rad_from_deg(X) ((X)*.0174532925)
 #define deg_from_rad(X) ((X)* 57.2957795)
 #define seconds_from_ms(x) ((x)/1000.0)
 #define DATA_RATE 16	//phidget pushes packets every 16 millisec
